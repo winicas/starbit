4fbc85441708fc915edd5c7440a58a33be31997b
winica <winicaloseka@gmail.com>
2025-05-21 17:48:32 +0100

    Dieu est vrais et le meme pourtoujoursffs

--- hopitalsage_front/app/login/page.tsx
+++ hopitalsage_front/app/login/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:8000/api/login/', {
+      const response = await axios.post('${process.env.NEXT_PUBLIC_API_URL}/api/login/', {
         username,
         password,
       });
